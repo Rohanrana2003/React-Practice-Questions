3caf0f4ec10d5ffb281104af0dee65594bfa9ccb
Rohanrana2003 <rohanrana33042@gmail.com>
2024-10-13 23:20:02 +0530

    Added UI changes

--- StyleSheet/src/App.jsx
+++ StyleSheet/src/App.jsx
@@ -1,34 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useRef } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [inputValue, setInputValue] = useState(0);
+
+
+  const handleBold = () =>{
+    console.log(inputValue);
+    setInputValue('hi')
+  }
+  const handleChagnge = (e) =>{
+    setInputValue(e.target.value);
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='container'>
+
+      <div className="grid">
+        <input  onChange={handleChagnge} type='text' />
+        <input  onChange={handleChagnge} type='text' />
+        <input  onChange={handleChagnge} type='text' />
+        <input  onChange={handleChagnge} type='text' />
+        <input  onChange={handleChagnge} type='text' />
+        <input  onChange={handleChagnge} type='text' />
+        <input  onChange={handleChagnge} type='text' />
+        <input  onChange={handleChagnge} type='text' />
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+      <div className="editing-buttons">
+        <button onClick={handleBold}>Bold</button>
+        <button>Strike Through</button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+    </div>
+
   )
 }
 
